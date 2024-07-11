00b5939ca8158083668b5a2ad34755d47b598402
Asadullah Galib <md.asadullah196@gmail.com>
2024-07-11 12:59:46 +0800

    header footer setup done

---
+++ themes/hexshop/footer.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * The footer of the Hexshop theme.
+ *
+ * This file serves as the footer template for Hexshop WordPress theme.
+ * It contains the opening HTML document structure, <footer> section, and
+ * the ending of the <body> section.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package Hexshop
+ * @since 1.0.0
+ */
+?>
+
+    <script>
+
+        $(function() {
+            var selectedClass = "";
+            $("p").click(function(){
+            selectedClass = $(this).attr("data-rel");
+            $("#portfolio").fadeTo(50, 0.1);
+                $("#portfolio div").not("."+selectedClass).fadeOut();
+            setTimeout(function() {
+                $("."+selectedClass).fadeIn();
+                $("#portfolio").fadeTo(50, 1);
+            }, 500);
+                
+            });
+        });
+
+    </script>
+
+    <?php wp_footer(); ?>
+</body>
+
+</html>
---
+++ themes/hexshop/header.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * The header of the hexshop theme.
+ *
+ * This file serves as the header template for hexshop WordPress theme.
+ * It contains the opening HTML document structure, <head> section, and
+ * the beginning of the <body> section.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package hexshop
+ * @since 1.0.0
+ */
+?>
+
+<!DOCTYPE html>
+<html <?php language_attributes(); ?> class="no-js">
+
+<head>
+    <meta charset="<?php bloginfo( 'charset' ); ?>">
+
+    <?php if (is_singular() && pings_open( get_queried_object() ) ) : ?>
+    <?php endif; ?>
+
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=5.0">
+    <meta name="author" content="<?php echo esc_attr( get_bloginfo( 'name' ) ); ?>">
+    <meta name="generator" content="<?php echo esc_attr( 'WordPress ' . get_bloginfo( 'version' ) ); ?>">
+
+    <?php wp_head(); ?>
+</head>
+
+<body <?php body_class(); ?>>
+    <?php //hexshop_header_parts(); ?>
