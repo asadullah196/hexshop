6d75794672fb74bc6660c8caecb66d43049b0d58
Asadullah Galib <md.asadullah196@gmail.com>
2024-07-11 13:11:31 +0800

    CSS and JS enqueue done

---
+++ themes/hexshop/funtions.php
@@ -0,0 +1,77 @@
+<?php
+/**
+ * Theme functions and definitions.
+ *
+ * This file sets up the theme and provides some helper functions.
+ * You can use it to make your theme more functional and customizable.
+ * Functions and definitions in this file can be used in your theme templates.
+ * A lot of other functional file has been linked in this file.
+ *
+ * @link https://developer.wordpress.org/themes/basics/theme-functions/
+ *
+ * @package hexshop
+ * @since 1.0.0
+ */
+
+// Exit if accessed directly.
+defined( 'ABSPATH' ) || exit;
+
+// hexshop theme support options calling here
+function hexshop_theme_support(){
+    
+    /** automatic feed link*/
+    add_theme_support( 'automatic-feed-links' );
+
+    /** tag-title **/
+    add_theme_support( 'title-tag' );
+
+    /** post thumbnail **/
+    add_theme_support( 'post-thumbnails' );
+
+    /** HTML5 support **/
+    add_theme_support( 'html5', array( 'comment-list', 'comment-form', 'search-form', 'gallery', 'caption' ) );
+
+    /** refresh widgest **/
+    add_theme_support( 'customize-selective-refresh-widgets' );
+
+    // hexshop all the menu
+    register_nav_menus( array(
+        'primary-menu' => __( 'Primary Menu', 'hexshop' ),
+        'social-menu' => __( 'Social Menu', 'hexshop' ),
+    ) );
+
+    /** post format **/
+    add_theme_support( 'post-formats', array( 
+            'image',
+            'audio',
+            'video',
+            'quote',
+            'gallery',
+        ) 
+    );
+
+    // D-register block editor
+    remove_theme_support( 'widgets-block-editor' );
+}
+add_action('after_setup_theme','hexshop_theme_support');
+
+// Plugin activation notice file calling
+
+// TGM plugin activation recommended plugin
+if ( file_exists( get_template_directory() . '/inc/init-plugins/init-hexshop-plugin.php' ) ) {
+    require_once( get_template_directory() . '/inc/init-plugins/init-hexshop-plugin.php' );
+}
+
+// All the theme script calling
+if( class_exists ( 'Kirki' ) ){
+	include_once('inc/common/kirki.php');
+}
+include_once('inc/common/scripts.php');
+
+/*
+include_once('inc/common/nav-walker.php');
+include_once('inc/common/breadcrumb.php');
+include_once('inc/common/comments-form-list.php');
+include_once('inc/common/template-functions.php');
+
+*/
---
+++ themes/hexshop/inc/common/scripts.php
@@ -0,0 +1,37 @@
+<?php
+
+define('HEXSHOP_CSS', get_template_directory_uri() . '/assets/css/');
+define('HEXSHOP_JS', get_template_directory_uri() . '/assets/js/');
+
+// hexshop CSS and JS enqueue here
+function hexshop_theme_scripts() {
+
+    // All CSS
+    wp_enqueue_style( 'bootstrap',  HEXSHOP_CSS.'bootstrap.min.css', array(), '1.0.0', 'all' );
+    wp_enqueue_style( 'font-awesome',  HEXSHOP_CSS.'font-awesome.css', array(), '1.0.0', 'all' );
+    wp_enqueue_style( 'owl-carousel',  HEXSHOP_CSS.'owl-carousel.css', array(), '1.0.0', 'all' );
+    wp_enqueue_style( 'lightbox',  HEXSHOP_CSS.'lightbox.css', array(), '1.0.0', 'all' );
+    wp_enqueue_style( 'hexshop-main',  HEXSHOP_CSS.'templatemo-hexashop.css', array(), '1.0.0', 'all' );
+	wp_enqueue_style( 'hexshop-style', get_stylesheet_uri() );
+
+    // All JS
+	wp_enqueue_script( 'popper', HEXSHOP_JS.'popper.js', array( ), '1.0.0', true );
+	wp_enqueue_script( 'bootstrap', HEXSHOP_JS.'bootstrap.min.js', array( ), '1.0.0', true );
+	wp_enqueue_script( 'owl-carousel', HEXSHOP_JS.'owl-carousel.js', array( ), '1.0.0', true );
+	wp_enqueue_script( 'accordions', HEXSHOP_JS.'accordions.js', array( ), '1.0.0', true );
+	wp_enqueue_script( 'datepicker', HEXSHOP_JS.'datepicker.js', array( ), '1.0.0', true );
+	wp_enqueue_script( 'scrollreveal', HEXSHOP_JS.'scrollreveal.min.js', array( ), '1.0.0', true );
+	wp_enqueue_script( 'waypoints', HEXSHOP_JS.'waypoints.min.js', array( ), '1.0.0', true );
+	wp_enqueue_script( 'jquery-counterup', HEXSHOP_JS.'jquery.counterup.min.js', array( ), '1.0.0', true );
+	wp_enqueue_script( 'imgfix', HEXSHOP_JS.'imgfix.min.js', array( ), '1.0.0', true );
+	wp_enqueue_script( 'slick', HEXSHOP_JS.'slick.js', array( ), '1.0.0', true );
+	wp_enqueue_script( 'lightbox', HEXSHOP_JS.'lightbox.js', array( ), '1.0.0', true );
+	wp_enqueue_script( 'isotope', HEXSHOP_JS.'isotope.js', array( ), '1.0.0', true );
+	wp_enqueue_script( 'hexshop-main', HEXSHOP_JS.'custom.js', array( 'jquery' ), 1.0, true );
+
+	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
+		wp_enqueue_script( 'comment-reply' );
+	}
+}
+
+add_action( 'wp_enqueue_scripts', 'hexshop_theme_scripts' );
