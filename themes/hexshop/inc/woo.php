d0afbc563e621ee07d4c4916fad24c5fc6d60d02
Asadullah Galib <md.asadullah196@gmail.com>
2024-07-16 11:29:23 +0800

    cart dynamic done

--- themes/hexshop/inc/woo.php
+++ themes/hexshop/inc/woo.php
@@ -15,6 +15,63 @@ remove_action('woocommerce_after_shop_loop_item_title','woocommerce_template_loo
 remove_action('woocommerce_after_shop_loop_item','woocommerce_template_loop_product_link_close',5);
 remove_action('woocommerce_after_shop_loop_item','woocommerce_template_loop_add_to_cart',10);
 
+// product add to cart button
+function hexshop_wooc_add_to_cart( $args = array() ) {
+    global $product;
+
+        if ( $product ) {
+            $defaults = array(
+                'quantity'   => 1,
+                'class'      => implode(
+                    ' ',
+                    array_filter(
+                        array(
+                            'product-add-cart-btn w-100 cart-button icon-btn button',
+                            'product_type_' . $product->get_type(),
+                            $product->is_purchasable() && $product->is_in_stock() ? 'add_to_cart_button' : '',
+                            $product->supports( 'ajax_add_to_cart' ) && $product->is_purchasable() && $product->is_in_stock() ? 'ajax_add_to_cart' : '',
+                        )
+                    )
+                ),
+                'attributes' => array(
+                    'data-product_id'  => $product->get_id(),
+                    'data-product_sku' => $product->get_sku(),
+                    'aria-label'       => $product->add_to_cart_description(),
+                    'rel'              => 'nofollow',
+                ),
+            );
+
+            $args = wp_parse_args( $args, $defaults );
+
+            if ( isset( $args['attributes']['aria-label'] ) ) {
+                $args['attributes']['aria-label'] = wp_strip_all_tags( $args['attributes']['aria-label'] );
+            }
+        }
+
+
+        // check product type 
+        if( $product->is_type( 'simple' ) ){
+            $btntext = esc_html__("Add to Cart",'hexshop');
+        } elseif( $product->is_type( 'variable' ) ){
+            $btntext = esc_html__("Select Options",'hexshop');
+        } elseif( $product->is_type( 'external' ) ){
+            $btntext = esc_html__("Buy Now",'hexshop');
+        } elseif( $product->is_type( 'grouped' ) ){
+            $btntext = esc_html__("View Products",'hexshop');
+        }
+        else{
+            $btntext = esc_html__("Add to Cart",'hexshop');
+        } 
+
+        echo sprintf( '<a href="%s" data-quantity="%s" class="%s" %s>%s</a>',
+            esc_url( $product->add_to_cart_url() ),
+            esc_attr( isset( $args['quantity'] ) ? $args['quantity'] : 1 ),
+            esc_attr( isset( $args['class'] ) ? $args['class'] : 'cart-button icon-btn button' ),
+            isset( $args['attributes'] ) ? wc_implode_html_attributes( $args['attributes'] ) : '',
+            '<i class="fa fa-shopping-cart"></i>'
+        );
+}
+
 // hexshop_product_grid
 function hexshop_product_grid(){
     global $post;
@@ -27,7 +84,9 @@ function hexshop_product_grid(){
                 <ul>
                     <li><a href="single-product.html"><i class="fa fa-eye"></i></a></li>
                     <li><a href="single-product.html"><i class="fa fa-star"></i></a></li>
-                    <li><a href="single-product.html"><i class="fa fa-shopping-cart"></i></a></li>
+                    <li>
+                        <?php hexshop_wooc_add_to_cart(); ?>
+                    </li>
                 </ul>
             </div>
             <a href="<?php the_permalink(); ?>"><?php the_post_thumbnail(); ?></a>
